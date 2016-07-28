ed2a87f51c92c5bb2f127dfad5fa4fbb02409fc1
Muhammad Hassan <hassantariq.8631@gmail.com>
2016-07-27 21:12:03 -0500

    AutoResizeDirectiveForAngularJS

---
+++ script.js
@@ -0,0 +1,35 @@
+﻿/**
+* THIS DIRECTIVE AUTOGROW TEXT-AREA
+* AS USER INPUT TEXT IN TEXT-AREA ON-FLY
+*/
+(function () {
+    'use strict';
+ 
+    angular
+        .module('sampleApp' , [])
+        .directive('autoResize', autoResize);
+ 
+    autoResize.$inject = ['$timeout'];
+ 
+    function autoResize($timeout) {
+ 
+        var directive = {
+            restrict: 'A',
+            link: function autoResizeLink(scope, element, attributes, controller) {
+ 
+                element.css({ 'height': 'auto', 'overflow-y': 'hidden' });
+                $timeout(function () {
+                    element.css('height', element[0].scrollHeight + 'px');
+                }, 100);
+ 
+                element.on('input', function () {
+                    element.css({ 'height': 'auto', 'overflow-y': 'hidden' });
+                    element.css('height', element[0].scrollHeight + 'px');
+ 
+                });
+            }
+        };
+ 
+        return directive;
+    };
+})();
